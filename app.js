d73111fa6880e275e59b382ea8db697d6ac714c7
Gabriel <gimenesgabriel732@gmail.com>
2026-03-04 22:00:05 -0300

    Finalizacao da Primeira Parte do Curso de Logico Com JS

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 alert("Boa vindas ao Jogo");
-let numeroSecreto = 4;
-console.log(numeroSecreto);
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random( ) * numeroMaximo + 1);
 let chute;
 let tentativas = 1;
 
 //enquanto chute nao for igual ao numero secreto
 while (chute != numeroSecreto ){
-        chute = prompt("Escolha um numero de 1 a 5 ");
+        chute = prompt(`Escolha um numero de 1 a ${numeroMaximo}`);
         //se chute for igal o numero secreto
     if (numeroSecreto == chute ){ 
         break;    
@@ -20,9 +20,12 @@ while (chute != numeroSecreto ){
 
     }
 }
-if (tentativas > 1){
-        alert(`Voce acertou o Numero, o numero era,  ${numeroSecreto} voce acertou em ${tentativas} chute` );
-} else {
-        alert(`Voce acertou o Numero, o numero era,  ${numeroSecreto} voce acertou em ${tentativas} chutes` );
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' ;
+ alert(`Voce acertou o Numero, o numero era,  ${numeroSecreto} voce acertou em ${tentativas} ${palavraTentativa}` );
 
-};
+//  if (tentativas > 1){
+//         alert(`Voce acertou o Numero, o numero era,  ${numeroSecreto} voce acertou em ${tentativas} chutes` );
+// } else {
+//         alert(`Voce acertou o Numero, o numero era,  ${numeroSecreto} voce acertou em ${tentativas} chute` );
+
+// };
